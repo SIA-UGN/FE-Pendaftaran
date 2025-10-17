77e8561182b2e13cbef61f32fdf465fb0b237d98
Faradis Yulianto <faradisy20@gmail.com>
2025-10-17 14:43:24 +0700

    feat: add registration progress component

--- src/app/(public)/pendaftaran/page.js
+++ src/app/(public)/pendaftaran/page.js
@@ -2,6 +2,7 @@ import ProtectedRoute from "@/components/ProtectedRoute";
 
 import InformasiPendaftaran from "@/components/InformasiPendaftaran";
 import InformasiProfil from "@/components/InformasiProfil";
+import RegistrationProgress from "@/components/RegistrationProgress"
 
 export default function PendaftaranPage() {
     return (
@@ -9,7 +10,7 @@ export default function PendaftaranPage() {
             <div className="py-5 flex items-center w-screen justify-center flex-col gap-5 max-w-11/12 mx-auto">
                 <InformasiPendaftaran />
                 <InformasiProfil />
-                {/* <RegistrationProgress /> */}
+                <RegistrationProgress />
             </div>
         </ProtectedRoute>
     )
