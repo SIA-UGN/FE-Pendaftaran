bd2f5f20fc74d78d530569fa7d9505e95e2634ef
Faradis Yulianto <faradisy20@gmail.com>
2025-10-23 19:03:55 +0700

    feat: fixing news component

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,7 +6,7 @@ import { NotebookPen } from "lucide-react"
 
 export default function Menu() {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-4 max-w-11/12 mx-auto my-3">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-4 max-w-11/12 mx-auto my-3 py-24">
             <Card className="flex items-center justify-center p-6 flex-col w-2/3 mx-auto">
                 <GraduationCap size={100} className="stroke-1"/>
                 Profil
--- src/components/News.js
+++ src/components/News.js
@@ -29,7 +29,7 @@ const news = [
 ];
 
 export default function News() {
-  const [data, setData] = useState(news); // langsung pakai data statis
+  const [data, setData] = useState(news);
 
   return (
     <div className="flex flex-col items-center pt-4 pb-16 px-4 sm:px-8 max-w-11/12 mx-auto">
@@ -39,8 +39,7 @@ export default function News() {
 
       <div className="flex flex-col gap-12 w-full">
         {data.map((item) => (
-          <Card key={item.title} className="flex flex-col md:flex-row w-full overflow-hidden p-0">
-            {/* Image wrapper */}
+          <Card key={item.title} className="flex flex-col md:flex-row w-full overflow-hidden p-0 min-h-60">
             <div className="relative w-full md:w-1/3 h-64 md:h-auto flex-shrink-0">
               <Image
                 src={item.image}
@@ -50,7 +49,6 @@ export default function News() {
               />
             </div>
 
-            {/* Content */}
             <div className="p-6 flex flex-col gap-4 md:w-2/3">
               <div className="flex justify-between items-start">
                 <h3 className="font-bold text-xl md:text-2xl">{item.title}</h3>
